dccba6361376046105a45c945e858d680b68da0b
Jose Quevin Ordoñez Sanchez <jose.ordonez@est.iudigital.edu.co>
2025-05-03 23:02:14 +0000

    empaquetado de dependencias

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find packages
+
+setup (
+    name="edu_pad",
+    version="0.0.1",
+    author="QUEVIN ORDONEZ",
+    author_email="jose.ordonez@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad1","actividad2"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "beautifulsoup4",
+    ],
+)
